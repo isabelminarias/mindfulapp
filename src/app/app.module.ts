7df5d70d8f117ab73f34c3d05ada4c74e1e97f34
Isabel M. Arias Salas <isabelminerva@outlook.com>
2017-11-25 12:55:00 -0400

    Modificated SCSS & some new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ClockComponent } from './clock/clock.component';
+import { TimerComponent } from './timer/timer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ClockComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
