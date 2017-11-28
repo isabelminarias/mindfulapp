582400e53ab454ef86fafee457705c0f47e7c4e9
Isabel M. Arias Salas <isabelminerva@outlook.com>
2017-11-28 08:50:33 -0400

    Aesthetic changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,14 +5,20 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
+  styleUrls: ['./about.component.scss']
   
 })
 export class AboutComponent implements OnInit {
+	
+	desc = [];
+  constructor() {
 
-  constructor() {}
+  	this.desc = ['This app was designed to let you practice mindfulness in a very convenient way. ',
+  	'Let your grateful thoughs of the day float in a cloud for a later, gloomy day. ',
+  	'Add pictures that will bring a smile to your face when you need it!'
+  ];
+  }
 
-  
   ngOnInit() {}
 
 }
